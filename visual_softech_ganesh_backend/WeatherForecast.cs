3d1aa27ec7bc7dcb4ede4b42b7a5e0a3a038f706
ganeshjoshii <ganeshkumarjoshiji@gmail.com>
2025-11-21 11:56:04 +0530

    Add project files.

---
+++ visual_softech_ganesh_backend/WeatherForecast.cs
@@ -0,0 +1,13 @@
+namespace visual_softech_ganesh_backend
+{
+    public class WeatherForecast
+    {
+        public DateOnly Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string? Summary { get; set; }
+    }
+}
